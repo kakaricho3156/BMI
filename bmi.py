84e775983754f6470854a23de989686f4096745e
kakaricho3156 <kakaricho1216@gmail.com>
2023-07-02 17:15:45 +0900

    BMI 完成

--- bmi.py
+++ bmi.py
@@ -1,18 +1,16 @@
 class BMI:
     def __init__(self, height, weight):
-        self.height = height
-        self.weight = weight
+        self.value = weight / (height**2)
+        if not (10 <= self.value <= 40):
+            raise ValueError("BMIの正常値を超えています")
 
-    def caluculate_bmi(self):
-        # bmiを計算するメソッド
-        return round(self.weight / ((self.height**2)), 2)
+    def __str__(self):
+        return f"{self.value:.2f}"
 
 
 # BMIクラスのインスタンス化
 tanaka_bmi = BMI(height=1.80, weight=67.0)
 sasaki_bmi = BMI(height=1.50, weight=77.0)
-print(tanaka_bmi.height)
-print(sasaki_bmi.weight)
+bob_bmi = BMI(height=1.70, weight=750000.0)
 
-print(tanaka_bmi.caluculate_bmi())
-print(sasaki_bmi.caluculate_bmi())
+print(tanaka_bmi)
